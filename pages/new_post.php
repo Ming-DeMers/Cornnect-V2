5bc3e6bf33f75711b23f8bb04f7e979a98108b00
Ming DeMers <mtd64@cornell.edu>
2023-04-17 17:24:55 -0400

    Updated form to be able to add images

--- pages/new_post.php
+++ pages/new_post.php
@@ -1,5 +1,17 @@
 <?php
 
+define("MAX_FILE_SIZE", 1000000);
+
+$upload_feedback = array(
+  'general_error' => False,
+  'type_error' => False,
+  'too_large' => False
+);
+
+// upload fields
+$upload_file_name = NULL;
+$upload_file_ext = NULL;
+
 // query the db
 $posts = exec_sql_query(
   $db,
@@ -21,6 +33,8 @@ $form_values = array(
   'netid' => '',
   'location' => '',
   'desc' => NULL,
+  'file_name' => '',
+  'file_ext' => ''
 );
 
 // validates if form was submitted:
@@ -28,6 +42,9 @@ if (isset($_POST['add-post'])) {
   $form_values['netid'] = trim($_POST['netid']);
   $form_values['location'] = trim($_POST['location']);
   $form_values['desc'] = trim($_POST['desc']);
+
+  // get the info about the uploaded files.
+  $upload = $_FILES['image'];
 
   $form_valid = True;
 
@@ -42,21 +59,43 @@ if (isset($_POST['add-post'])) {
     $form_feedback_classes['location'] = '';
   }
 
+  if ($upload['error'] == UPLOAD_ERR_OK) {
+    $upload_file_name = basename($upload['name']);
+    $upload_file_ext = strtolower(pathinfo($upload_file_name, PATHINFO_EXTENSION));
+  } else if (($upload['error'] == UPLOAD_ERR_INI_SIZE) || ($upload['error'] == UPLOAD_ERR_FORM_SIZE)) {
+    $form_valid = False;
+    $upload_feedback['too_large'] = True;
+  } else if (!in_array($upload_file_ext, array("png", "jpg"))) {
+    $form_valid = False;
+    $upload_feedback['type_error'] = True;
+  } else {
+    // upload was not successful
+    $form_valid = False;
+    $upload_feedback['general_error'] = True;
+  }
+
   // show confirmation if form is valid, otherwise set sticky values and echo them
   if ($form_valid) {
     exec_sql_query(
       $db,
-      "INSERT INTO posts (netid, location, desc, date) VALUES (:netid, :location, :desc, :date);",
+      "INSERT INTO posts (netid, location, desc, date, file_name, file_ext) VALUES (:netid, :location, :desc, :date, :file_name, :file_ext);",
       array(
         ':netid' => $form_values['netid'],
         ':location' => $form_values['location'],
         ':desc' => $form_values['desc'],
-        ':date' => date('Y-m-d H:i:s')
+        ':date' => date('Y-m-d H:i:s'),
+        'file_name' => $upload_file_name,
+        'file_ext' => $upload_file_ext
       )
     );
     $retry_form = False;
     $show_confirmation = True;
     $show_form = False;
+    $record_id = $db->lastInsertId('id');
+    $upload_storage_path = 'public/uploads/posts/' . $record_id . '.' . $upload_file_ext;
+    if (move_uploaded_file($upload["tmp_name"], $upload_storage_path) == False) {
+      error_log("Failed to permanently store the uploaded file on the file server. Please check that the server folder exists.");
+    }
   } else {
     $retry_form = True;
     $sticky_values['netid'] = $form_values['netid'];
@@ -84,27 +123,43 @@ if (isset($_POST['add-post'])) {
 
       <h2>Add new post!</h2>
       <?php if ($show_form) { ?>
-        <form method="post" action="/new_post" novalidate>
+        <form method="post" action="/new_post" enctype="multipart/form-data" novalidate>
           <p class="feedback <?php echo $form_feedback_classes['netid']; ?>">Please provide your netID</p>
           <div class="label-input">
             <label for="netid_field">netID:</label>
             <input id="netid_field" type="text" name="netid" value="<?php echo $sticky_values['netid']; ?>">
           </div>
-          <!-- <p class="feedback <?php echo $form_feedback_classes['netid']; ?>">Please provide an image</p> -->
-          <div class="label-input">
-            <label for="image_field">Image:</label>
-            <input id="image_field" type="text" name="image" value="<?php echo $sticky_values['image']; ?>">
-          </div>
+
           <p class="feedback <?php echo $form_feedback_classes['location']; ?>">Where are you?</p>
           <div class="label-input">
             <label for="location_field">Location:</label>
             <input id="location_field" type="text" name="location" value="<?php echo $sticky_values['location']; ?>">
           </div>
+
+
+          <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo MAX_FILE_SIZE; ?>">
+
+          <?php if ($upload_feedback['too_large']) { ?>
+            <p class="feedback">Files must be less than 1MB.</p>
+          <?php } ?>
+          <?php if ($upload_feedback['type_error']) { ?>
+            <p class="feedback">We only accept jpg or img files.</p>
+          <?php } ?>
+          <?php if ($upload_feedback['general_error']) { ?>
+            <p class="feedback">Please provide an image.</p>
+          <?php } ?>
+
+          <div class="label-input">
+            <label for="upload-file">Image:</label>
+            <input id="upload-file" type="file" name="image">
+          </div>
+
           <div class="label-input">
             <label for="desc_field">Description:</label>
             <input id="desc_field" type="text" name="desc" value="<?php echo $sticky_values['desc']; ?>">
           </div>
-          <div class="add-button">
+
+          <div class=" add-button">
             <input type="submit" value="Add post!" name="add-post">
           </div>
         </form>
@@ -117,7 +172,7 @@ if (isset($_POST['add-post'])) {
   <div class="confirmation">
     <section>
       <h3>Post Pubbed!</h3>
-      <p>Thank you <?php echo htmlspecialchars($form_values['netid']); ?>. Your post has been added!</p>
+      <p>Thank you <?php echo htmlspecialchars($form_values['netid']); ?>. Your post has been added! You used the photo <?php echo $upload_file_name; ?> with the extension <?php echo $upload_file_ext; ?></p>
       <?php $show_form = FALSE ?>
     </section>
   </div>
